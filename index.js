2d31977936646c742bfb3c0203b43d9136b08ae0
Roma <sikarev.roman@yandex.ru>
2021-05-15 12:48:23 +0300

    todo-list api, get + post query in one programm

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 const resultBlock = document.querySelector("#result");
-const clickMeButton = document.querySelector("#click-me");
 const pageNumber = document.querySelector("#page-number");
+const clickMeButton = document.querySelector("#click-me");
+const getTasksButton = document.querySelector("#get-tasks");
+
 clickMeButton.addEventListener("click", () => {
     const promise = getImages(pageNumber);
-    promise.then(onDataRecieved);
+    promise.then(onImagesRecieved);
 });
 
-function onDataRecieved (data) {
-    data.forEach(el => {
+function onImagesRecieved (array) {
+    array.forEach(el => {
         const img = document.createElement('img');
         img.src = el.thumbnail;
         document.querySelector('#result').appendChild(img);
+    })
+}
+
+getTasksButton.addEventListener("click", () => {
+    const promise = getTasks();
+    promise.then(onTasksRecieved);
+});
+
+createTasks('learn JS').then((data) => {
+    console.log(data);
+});
+
+function onTasksRecieved (array) {
+    array.forEach(el => {
+        const li = document.createElement('li');
+        li.innerHTML = el.title;
+        document.querySelector('#task-result').appendChild(li);
     })
 }
--- service.js
+++ service.js
@@ -1,10 +1,19 @@
-// function getImages(pageNumber) {
-//     let promise = $.ajax(`https://repetitora.net/api/JS/Images?page=${pageNumber.value}&count=1`);
-//     return promise;
-// }
-
 function getImages(pageNumber) {
     let promise = axios.get(`https://repetitora.net/api/JS/Images?page=${pageNumber.value}&count=1`);
+    return promise.then((response) => {
+        return response.data;
+    });
+}
+
+function getTasks() {
+    let promise = axios.get(`https://repetitora.net/api/JS/Tasks?widgetId=534235`);
+    return promise.then((response) => {
+        return response.data;
+    });
+}
+
+function createTasks(title) {
+    const promise = axios.post(`https://repetitora.net/api/JS/Tasks?widgetId=534235&title=${title}`);
     return promise.then((response) => {
         return response.data;
     });
